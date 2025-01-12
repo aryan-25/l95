05147d4290187657eda6e86f254f50a3de7a071f
Aryan Shah <aryan25@gmail.com>
2025-01-12 19:30:05 +0000

    Update experiments

--- experiments.py
+++ experiments.py
@@ -19,8 +19,8 @@ from parser_loader.constituency.parsers import (
 from parser_loader.dependency.parsers import SpacyDependencyParser
 
 
-def get_sentences() -> list[gold_standard_loader.Sentence]:
-    return gold_standard_loader.parse("gold_standard/modified_gold_standard.txt")
+def get_sentences(gold_standard_filename: str) -> list[gold_standard_loader.Sentence]:
+    return gold_standard_loader.parse(gold_standard_filename)
 
 
 def display_constituency_parses(*parses: list[Tree]):
@@ -52,23 +52,26 @@ def display_dependency_parses(doc: Union[list[Doc], list[dict]], manual=False, p
 
 
 def constituency_parsers():
-    sentences = get_sentences()
+    gold = get_sentences("gold_standard/gold_standard.txt")
+    modified_gold = get_sentences("gold_standard/modified_gold_standard.txt")
 
-    coreNLP_parses = CoreNLPConstituencyParser().parse_multiple(sentences)
-    # stanza_parses = StanzaConstituencyParser().parse_multiple(sentences)
-    # berkeley_parses = BerkeleyConstituencyParser().parse_multiple(sentences)
-    berkeley_neural_parses = BerkeleyNeuralConstituencyParser().parse_multiple(sentences)
-    gold_parses = [sentence.constituency_parse.nltk_tree for sentence in sentences]
+    coreNLP_parses = CoreNLPConstituencyParser().parse_multiple(modified_gold)
+    # stanza_parses = StanzaConstituencyParser().parse_multiple(modified_gold)
+    # berkeley_parses = BerkeleyConstituencyParser().parse_multiple(modified_gold)
+    berkeley_neural_parses = BerkeleyNeuralConstituencyParser().parse_multiple(modified_gold)
+    gold_parses = [sentence.constituency_parse.nltk_tree for sentence in gold]
+    modified_gold_parses = [sentence.constituency_parse.nltk_tree for sentence in modified_gold]
 
     parses = {
         "coreNLP": coreNLP_parses,
         # "stanza": stanza_parses,
         # "berkeley": berkeley_parses,
         "benepar": berkeley_neural_parses,
-        "modified_gold": gold_parses,
+        "gold": gold_parses,
+        "modified_gold": modified_gold_parses,
     }
 
-    for i in range(len(sentences)):
+    for i in range(len(modified_gold)):
         for parser, parser_parses in parses.items():
             if not os.path.exists(f"parser_output/{parser}"):
                 os.makedirs(f"parser_output/{parser}")
@@ -80,11 +83,13 @@ def constituency_parsers():
             if not os.path.exists(f"parser_output/evalb_results/sentence_{i + 1}"):
                 os.makedirs(f"parser_output/evalb_results/sentence_{i + 1}")
 
-            if parser == "modified_gold":
+            if parser == "modified_gold" or parser == "gold":
                 continue  # to skip evaluation of the gold standard against itself
 
-            evaluation_cmd = f"./evaluation_tools/EVALB/evalb parser_output/modified_gold/sentence_{i + 1}.txt parser_output/{parser}/sentence_{i + 1}.txt > parser_output/evalb_results/sentence_{i + 1}/{parser}_vs_gold.txt"
-            os.system(evaluation_cmd)
+            gold_evaluation_cmd = f"./evaluation_tools/EVALB/evalb parser_output/original_gold/sentence_{i + 1}.txt parser_output/{parser}/sentence_{i + 1}.txt > parser_output/evalb_results/sentence_{i + 1}/{parser}_vs_original_gold.txt"
+            os.system(gold_evaluation_cmd)
+            modified_gold_evaluation_cmd = f"./evaluation_tools/EVALB/evalb parser_output/modified_gold/sentence_{i + 1}.txt parser_output/{parser}/sentence_{i + 1}.txt > parser_output/evalb_results/sentence_{i + 1}/{parser}_vs_modfied_gold.txt"
+            os.system(modified_gold_evaluation_cmd)
 
         # Use Benepar as the gold standard reference and evaluate CoreNLP against it
         rel_evaluation_cmd = f"./evaluation_tools/EVALB/evalb parser_output/benepar/sentence_{i + 1}.txt parser_output/coreNLP/sentence_{i + 1}.txt > parser_output/evalb_results/sentence_{i + 1}/coreNLP_vs_benepar.txt"
@@ -94,12 +99,12 @@ def constituency_parsers():
     display_constituency_parses(
         coreNLP_parses,
         berkeley_neural_parses,
-        gold_parses,
+        modified_gold_parses,
     )
 
 
 def dependency_parsers():
-    sentences = get_sentences()
+    sentences = get_sentences("gold_standard/gold_standard.txt")
 
     spacy_parses = SpacyDependencyParser().parse_multiple([sentence.text for sentence in sentences])
     gold_parses: list[gold_standard_loader.DependencyParse] = [sentence.dependency_parse for sentence in sentences]
